9f97f6fa7a3bebf94e296ca1bc24e3fad7678da0
aleix <aleix.ordeig@gmail.com>
2024-08-01 16:11:23 -0600

    update footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -137,10 +137,6 @@ const config = {
           {
             title: 'Community',
             items: [
-              // {
-              //   label: 'Stack Overflow',
-              //   href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              // },
               {
                 label: 'Discord',
                 href: 'https://discord.gg/2vDMR8m',
@@ -152,15 +148,27 @@ const config = {
             ],
           },
           {
-            title: 'More',
+            title: 'Official Links',
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
+                label: 'Careers',
+                href: 'https://qubic.org/careers',
+              },
+              {
+                label: 'Explorer',
+                href: 'https://explorer.qubic.org/',
               },
               {
                 label: 'GitHub',
                 href: 'https://github.com/qubic/docs',
+              },
+              {
+                label: 'Qubic Website',
+                href: 'https://qubic.org/',
+              },
+              {
+                label: 'Wallet',
+                href: 'https://wallet.qubic.org/',
               },
             ],
           },
